049487eef6ef99e762456c2cff246684e5c4988c
Mansam <mansam8@hotmail.com>
2021-02-02 11:02:43 +0300

    small changes in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>SHAMBA MARKET</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -18,11 +18,76 @@
             body {
                 font-family: 'Nunito';
             }
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .links a:hover {
+                color: orange;
+                font-weight: bold;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
         </style>
     </head>
-    <body class="antialiased">
-    <h1>SHAMBA MARKET</h1>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    SHAMBA MARKET
+                </div>
+
+            </div>
+        </div>
     </body>
+    <!--<body class="antialiased">
+    <h1>SHAMBA MARKET</h1>
+    </body>-->
 </html>
 
 
