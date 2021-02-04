34a18ba78ac1ee0115a675bf16ef6605393d4a79
Angelo Thomas <angelobusee@gmail.com>
2021-02-04 08:46:11 +0300

    Contact updated

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,10 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::get('/contact', function(){
+    return view('frontend.contact');
+});
+
 
 Route::group(['middleware' => ['auth', 'isAdmin']],function(){
     Route::get('/admin', function () {
