57e3c932f9ca494900385aa2be40da3a4af29cac
Richard James <remodsouza865@gmail.com>
2021-02-18 18:39:35 -0500

    Modified Vendor panel

--- resources/views/layouts/vendor.blade.php
+++ resources/views/layouts/vendor.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vendor Panel</title>
+    <title>Vendor-Panel</title>
      <!-- Scripts -->
      <script src="{{ asset('js/app.js') }}" defer></script>
 
