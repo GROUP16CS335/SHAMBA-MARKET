99ced8e4d368317b1f5f143f1fcfe60d0e657c08
Angelo Thomas <angelobusee@gmail.com>
2021-02-19 12:39:03 +0300

    Update home

--- resources/views/layouts/inc/footer.blade.php
+++ resources/views/layouts/inc/footer.blade.php
@@ -7,9 +7,11 @@
                 <div class="col-lg-4 col-md-12 col-sm-12">
                     <div class="footer-widget">
                         <h4>About SHAMBA-MARKET</h4>
-                        <p>Shamba market is a web based application which based more
-                            on selling crops online and make a delivery to people
-                    </p>
+                        <p>Shamba Market is a web-based application that helps to connect between the farmers and buyers/suppliers.
+                             This web-based application deals more on selling of agriculture product,
+                            looking for the market of different agriculture product like organic food,
+                            providing information about the price fluctuation on different crops and also to
+                            solve different problem that facing different farmers in agricultural sectors</p>
                         </div>
                 </div>
                 <div class="col-lg-4 col-md-12 col-sm-12">
